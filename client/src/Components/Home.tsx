7300daac72a67fb3e2c4eeef8c860359f0cd9fd1
MRombi <michael.rombi@hotmail.com>
2022-08-15 16:22:50 +0100

    add a field to our signup on Auth0

--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -1,24 +1,18 @@
-import {Button} from '@mui/material';
-import React from 'react';
-import {useAuth0} from '@auth0/auth0-react';
-
-// const {user } = useAuth0()
-
-
+import { Button } from "@mui/material";
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Home = () => {
-  // const {user } = useAuth0()
- ;
+  return (
+    <div className="HomePage">
+      <Button variant="outlined" size="large">
+        Interactive Map
+      </Button>
+      <Button variant="outlined" size="large">
+        Aroun The Word
+      </Button>
+    </div>
+  );
+};
 
-    return (
-      
-      <div className="HomePage">
-        
-        <Button variant="outlined" size='large'>Interactive Map</Button>
-        <Button variant="outlined" size='large'>Aroun The Word</Button>
-      </div>
-    );
-  }
-
-
-  export default Home
+export default Home;
