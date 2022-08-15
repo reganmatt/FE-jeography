bdae84244d0da1997003dea4a3df34da15ee1a60
reganmatt <104975201+reganmatt@users.noreply.github.com>
2022-08-15 09:58:29 +0100

    Update Header.tsx

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -3,7 +3,7 @@ import AuthNav from './AuthNav';
 
 
 const Header: React.FC = () => {
-    return (
+    return  (
       <div className="header">
        <img src="https://i.imgur.com/Xc5ByTY.png" alt="jeo-logo" className="jeo-logo"></img>
        <img src="https://i.imgur.com/HuSyK8h.png" alt="jeo-banner" className="jeo-banner"></img>  
