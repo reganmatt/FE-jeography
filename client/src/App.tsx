1dd348739e24310dc249fea4b4a6cf3ecde72220
MRombi <michael.rombi@hotmail.com>
2022-08-15 11:02:43 +0100

    solve router issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,11 @@ import Home from "./Components/Home";
 import "./css/App.scss";
 
 const App: React.FC = () => {
-  console.log(process.env)
   return (
     <div className="App">
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
         </Routes>
-      </BrowserRouter>
     </div>
   );
 };
