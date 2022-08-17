828e68e7c505b91667532dc8b7b8a6d3d67e1342
Jadwiga <grywalska@gmail.com>
2022-08-17 15:08:50 +0100

    student profile updated

--- client/src/Components/StudentProfile.tsx
+++ client/src/Components/StudentProfile.tsx
@@ -1,12 +1,11 @@
-import {Button} from "@mui/material";
+import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
 import * as api from "../api";
 import { Students } from "../Types/types";
 import { useParams, Link } from "react-router-dom";
 //import '../cssnkQuiz.scss';
 
-
-const StudentProfile = () => {   
+const StudentProfile = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [student, setStudent] = useState<Students[]>([]);
   let nickname = useParams().nickname;
@@ -14,42 +13,54 @@ const StudentProfile = () => {
   useEffect(() => {
     if (nickname !== undefined) {
       api.getStudentByUsername(nickname).then((student) => {
-        console.log(student, "in the student")
+        console.log(student, "in the student");
         setStudent(student);
         setIsLoading(false);
       });
     }
-    
   }, [nickname]);
 
-  
-    return(
-      <section >
+  if (isLoading === true) {
+    return <p>Loading ...</p>;
+  } else {
+    return (
+      <section>
         <section>
-          <h1>Hello there, {nickname}!</h1>          
-        </section>        
-        <section >                
-          {student.map((student, index) => ( 
-          <div key={index} className="student-item">           
-            <h5 className="box-caption"><Link to={`/students/${student.username}`}>{student.username}</Link></h5>
-            <p>You've collected {student.userPoints} Jeo points</p>
-          </div>
-        ))}
+          <h1>Hello there, {nickname}!</h1>
+        </section>
+        <section>
+          {student.map((student, index) => (
+            <div key={index} className="student-item">
+              <h5 className="box-caption">
+                <Link to={`/students/${student.username}`}>
+                  {student.username}
+                </Link>
+              </h5>
+              <p>You've collected {student.userPoints} Jeo points</p>
+            </div>
+          ))}
         </section>
         <section>
           <p>Jeo Ranch</p>
           <p>Select a Jeo badge to replace your avatar!</p>
-          {student[0].jeoRanch.map((img, index) => (            
-            <img src={img} alt="studentAvatar" className="avatar" width="100" height="100" />      
+          {student[0].jeoRanch.map((img, index) => (
+            <img
+              src={img}
+              alt="studentAvatar"
+              className="avatar"
+              width="100"
+              height="100"
+            />
           ))}
-        </section>     
-          <Link to="/">
-            <Button size="small" variant="contained">
-              Take a quiz
-            </Button>
-          </Link>    
-      </section>  
-    )  
+        </section>
+        <Link to="/select_quiz">
+          <Button size="small" variant="contained">
+            Take a quiz
+          </Button>
+        </Link>
+      </section>
+    );
+  }
 };
 
 export default StudentProfile;
